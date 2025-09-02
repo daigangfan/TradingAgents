59b1f6b735e22c9e91c81bd69cc9ec358d0ec8bc
daigangfan <daigangfan01@163.com>
2025-09-02 21:44:36 +0800

    fix embedding into gemini

--- tradingagents/agents/utils/memory.py
+++ tradingagents/agents/utils/memory.py
@@ -1,15 +1,15 @@
 import chromadb
 from chromadb.config import Settings
 from openai import OpenAI
-
+import os 
 
 class FinancialSituationMemory:
     def __init__(self, name, config):
         if config["backend_url"] == "http://localhost:11434/v1":
             self.embedding = "nomic-embed-text"
         else:
-            self.embedding = "text-embedding-v4"
-        self.client = OpenAI(base_url=config["backend_url"])
+            self.embedding = "gemini-embedding-001"
+        self.client = OpenAI(base_url=config["backend_url"],api_key=os.environ['EMBEDDING_API_KEY'])
         self.chroma_client = chromadb.Client(Settings(allow_reset=True))
         self.situation_collection = self.chroma_client.create_collection(name=name)
 
